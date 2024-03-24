099b85bdba1f37aee3bb1a198148b9757809043f
dusanMatijevic <dusanm2401@gmail.com>
2024-03-24 21:50:56 +0100

    Initiall push

---
+++ NsiProjekat1TestSdk/NsiProjekat1TestSdk/Controllers/CompanyController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NsiProjekat1Sdk.Sdk;
+using NsiProjekat1Sdk.Application.Client;
+using NsiProjekat1Sdk.Application.Models;
+using NsiProjekat1Sdk.Sdk.Dto;
+using Refit;
+
+namespace NsiProjekat1TestSdk.Controllers;
+
+public class CompanyController() : ControllerBase
+{
+    [HttpPost("create")]
+    public async Task<IActionResult> Create(NsiProjekat1SdkCompanyCreateDto company)
+    {
+        var httpClient = new HttpClient
+        {
+            BaseAddress = new Uri("http://localhost:5140"),
+        };
+
+        var api = RestService.For<INsiProjekat1SdkApi>(httpClient);
+        var client = new NsiProjekat1SdkClient(api);
+        
+        var headers = new Dictionary<string, string> 
+        {
+            {"X-De-Username", "dusan"},
+            {"X-De-Password", "test"}
+        };
+        
+        var result = await client.CreateCompanyAsync(new NsiProjekat1SdkCompanyRequestModel(
+            company.Name,
+            "http://localhost:5140", 
+            headers));
+        return Ok(result);
+    }
+}
---
+++ NsiProjekat1TestSdk/NsiProjekat1TestSdk/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NsiProjekat1Sdk.Sdk;
+using NsiProjekat1Sdk.Application.Client;
+using NsiProjekat1Sdk.Application.Models;
+using NsiProjekat1Sdk.Sdk.Dto;
+using Refit;
+
+namespace NsiProjekat1TestSdk.Controllers;
+
+public class ProductController() : ControllerBase
+{
+    [HttpPost("create")]
+    public async Task<IActionResult> Create(NsiProjekat1SdkProductCreateDto product)
+    {
+        var httpClient = new HttpClient
+        {
+            BaseAddress = new Uri("http://localhost:5140"),
+        };
+
+        var api = RestService.For<INsiProjekat1SdkApi>(httpClient);
+        var client = new NsiProjekat1SdkClient(api);
+        
+        var headers = new Dictionary<string, string> 
+        {
+            {"X-De-Username", "dusan"},
+            {"X-De-Password", "test"}
+        };
+        
+        var result = await client.CreateProductAsync(new NsiProjekat1SdkProductRequestModel(
+            product.CompanyId, 
+            product.Name,
+            product.Description,
+            "http://localhost:5140", 
+            headers));
+        return Ok(result);
+    }
+}
